47433e2d5a42054d3cac29abdb1ade9efc25403f
cjwddz <cjwddz@qq.com>
2017-10-11 17:15:03 +0800

    add something!

--- src/app/services/meditor.service.ts
+++ src/app/services/meditor.service.ts
@@ -1,1 +1,20 @@
-s
+
+
+
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs/Subject';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+export class MeditorService {
+  private subject = new Subject<{id: string, body: any}>();
+  constructor() {}
+  // 获取订阅者
+  public getObservable (): Observable<{id: string, body: any}> {
+    return this.subject.asObservable();
+  }
+  // 发送信息
+  public push(msg: {id: string, body: any}) {
+    this.subject.next(msg);
+  }
+}
